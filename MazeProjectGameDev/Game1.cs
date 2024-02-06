bac101cb4e35f67afc78671d10da6b3953cc04d3
Dingleberryipod <a02330716@usu.edu>
2024-02-05 22:52:17 -0700

    added boundary for maze

--- MazeProjectGameDev/Game1.cs
+++ MazeProjectGameDev/Game1.cs
@@ -38,6 +38,7 @@ namespace MazeProjectGameDev
         private Texture2D endTexture;
 
         private GraphNode playerNode;
+        private Rectangle mazeRectangle;
 
         private const int GAME_MOVEMENT = 1;
 
@@ -60,6 +61,7 @@ namespace MazeProjectGameDev
 
         protected override void Initialize()
         {
+
             m_graphics.PreferredBackBufferWidth = 1920;
             m_graphics.PreferredBackBufferHeight = 1080;
             m_graphics.ApplyChanges();
@@ -70,22 +72,17 @@ namespace MazeProjectGameDev
             GameWidth = 20;
             GameHeight = 20;
             playerIndex = 0;
+            mazeRectangle = new Rectangle(m_graphics.PreferredBackBufferWidth / 8, m_graphics.PreferredBackBufferHeight / 8, m_graphics.PreferredBackBufferWidth - m_graphics.PreferredBackBufferWidth / 4, m_graphics.PreferredBackBufferHeight - m_graphics.PreferredBackBufferHeight / 4);
             rectangles = new List<Rectangle>();
             isMouseOn = new List<bool>();
-            m_myBox = new Rectangle(m_graphics.PreferredBackBufferWidth / GameWidth / 4, m_graphics.PreferredBackBufferHeight / GameWidth / 4, m_graphics.PreferredBackBufferWidth / GameWidth / 2, m_graphics.PreferredBackBufferHeight / GameHeight / 2);
-            backGroundBoth = this.Content.Load<Texture2D>("backgroundBoth");
-            backGroundBottom = this.Content.Load<Texture2D>("backgroundBottom");
-            backGroundRight = this.Content.Load<Texture2D>("backgroundRight");
-            backGroundDefault = this.Content.Load<Texture2D>("mazeBackgroundNone");
-            endTexture = this.Content.Load<Texture2D>("IMG_2092");
-            playerTexture = this.Content.Load<Texture2D>("saulgoodman");
-            shortestPathTexture = this.Content.Load<Texture2D>("pixilMoney");
+            m_myBox = new Rectangle(mazeRectangle.X + mazeRectangle.Width / GameWidth / 4, mazeRectangle.Y + mazeRectangle.Height / GameHeight / 4, mazeRectangle.Width / GameWidth / 2, mazeRectangle.Height / GameHeight / 2);
+            
             for (int i = 0; i < GameHeight; i++)
             {
                 for (int j = 0; j < GameWidth; j++)
                 {
                     isMouseOn.Add(false);
-                    rectangles.Add(new Rectangle(m_graphics.PreferredBackBufferWidth / GameWidth * j, m_graphics.PreferredBackBufferHeight / GameHeight * i, m_graphics.PreferredBackBufferWidth / GameWidth, m_graphics.PreferredBackBufferHeight / GameHeight));
+                    rectangles.Add(new Rectangle(mazeRectangle.X + mazeRectangle.Width / GameWidth * j, mazeRectangle.Y + mazeRectangle.Height / GameHeight * i, mazeRectangle.Width / GameWidth, mazeRectangle.Height / GameHeight));
                 }
             }
             maze = new Maze(GameWidth, GameHeight, 0);
@@ -99,6 +96,13 @@ namespace MazeProjectGameDev
 
         protected override void LoadContent()
         {
+            backGroundBoth = this.Content.Load<Texture2D>("backgroundBoth");
+            backGroundBottom = this.Content.Load<Texture2D>("backgroundBottom");
+            backGroundRight = this.Content.Load<Texture2D>("backgroundRight");
+            backGroundDefault = this.Content.Load<Texture2D>("mazeBackgroundNone");
+            endTexture = this.Content.Load<Texture2D>("IMG_2092");
+            playerTexture = this.Content.Load<Texture2D>("saulgoodman");
+            shortestPathTexture = this.Content.Load<Texture2D>("pixilMoney");
             m_spriteBatch = new SpriteBatch(GraphicsDevice);
             pixel = new Texture2D(GraphicsDevice, 5, 5);
             Color[] colors = new Color[5 * 5];
@@ -172,7 +176,7 @@ namespace MazeProjectGameDev
                     }
                     playerNode = playerNode.getRight();
                     playerIndex += 1;
-                    m_myBox.X += m_graphics.PreferredBackBufferWidth / GameWidth;
+                    m_myBox.X += mazeRectangle.Width / GameWidth;
 
                 }
 
@@ -204,7 +208,7 @@ namespace MazeProjectGameDev
                         }
                         playerNode = playerNode.getLeft();
                         playerIndex -= 1;
-                        m_myBox.X -= m_graphics.PreferredBackBufferWidth / GameWidth;
+                        m_myBox.X -= mazeRectangle.Width / GameWidth;
 
                     }
 
@@ -236,7 +240,7 @@ namespace MazeProjectGameDev
                     }
                     playerNode = playerNode.getTop();
                     playerIndex -= GameWidth;
-                    m_myBox.Y -= m_graphics.PreferredBackBufferHeight / GameHeight;
+                    m_myBox.Y -= mazeRectangle.Height / GameHeight;
 
 
                 }
@@ -268,7 +272,7 @@ namespace MazeProjectGameDev
                     isButtonPressed = true;
                     playerNode = playerNode.getBottom();
                     playerIndex += GameWidth;
-                    m_myBox.Y += m_graphics.PreferredBackBufferHeight / GameHeight;
+                    m_myBox.Y += mazeRectangle.Height / GameHeight;
 
                 }
 
@@ -374,7 +378,7 @@ namespace MazeProjectGameDev
             for(int i = 0; i < maze.locations.Count; i++)
             {
                 Console.WriteLine("Yeah");
-                Rectangle tempRec = new Rectangle(rectangles[maze.locations[i]].X + m_graphics.PreferredBackBufferWidth / GameWidth / 4, rectangles[maze.locations[i]].Y + m_graphics.PreferredBackBufferHeight / GameWidth / 4, m_graphics.PreferredBackBufferWidth / GameWidth / 2, m_graphics.PreferredBackBufferHeight / GameHeight / 2);
+                Rectangle tempRec = new Rectangle(rectangles[maze.locations[i]].X + mazeRectangle.Width / GameWidth / 4, rectangles[maze.locations[i]].Y + mazeRectangle.Height / GameWidth / 4, mazeRectangle.Width / GameWidth / 2, mazeRectangle.Height / GameHeight / 2);
                 m_spriteBatch.Draw(shortestPathTexture, tempRec, Color.White);
 
 
@@ -434,6 +438,9 @@ namespace MazeProjectGameDev
 
 
             }*/
+
+            //m_spriteBatch.Draw(playerTexture, mazeRectangle, Color.White);
+
 
             m_spriteBatch.End();
             base.Draw(gameTime);
